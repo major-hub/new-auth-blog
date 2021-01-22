801cbc4261d381e0ed146bf23e2c75caf029b76a
major-hub <https://major-hub@github.com>
2021-01-22 15:39:55 +0500

    add .env file

--- itproger/settings.py
+++ itproger/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
 
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,9 +133,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog:home'
 LOGIN_URL = 'login' # bu kerak emas agar @login_required(login_url='login') qilib yozsak
 
-EMAIL_HOST = 'smtp.mail.ru'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = 'otabeksolijonov@mail.ru'
-EMAIL_HOST_PASSWORD = '<password>'
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+load_dotenv()
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv("GMAIL")
+EMAIL_HOST_PASSWORD = os.getenv("PASSWORD")
+EMAIL_USE_TLS = True
--- itproger/urls.py
+++ itproger/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('password-reset-confirm/<uidb64>/<token>/',
          PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
          name='password_reset_confirm'),
-    path('password-reset/done/',
+    path('password-reset-done/',
          PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
          name='password_reset_done'),
     path('', include('blog.urls')),
